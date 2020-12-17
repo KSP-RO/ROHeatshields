7940baa00cd785024a786e50d28ab4f71f1ac49a
Standecco <dlombardo32@gmail.com>
2020-12-17 01:00:31 +0100

    Persist ablator amount, add cost scaling

--- Source/ModuleROHeatshield.cs
+++ Source/ModuleROHeatshield.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using ROLib;
+using System;
 
 namespace ROHeatshields
 {
-    public class ModuleROHeatshield : PartModule, IPartMassModifier
+    public class ModuleROHeatshield : PartModule, IPartMassModifier, IPartCostModifier
     {
         private const string GroupDisplayName = "RO-Heatshields";
         private const string GroupName = "ModuleROHeatshield";
@@ -15,6 +16,9 @@ namespace ROHeatshields
 
         [KSPField] public float heatShieldDensity = 0.0f;
         [KSPField] public float heatShieldAblator = 0.0f;
+        [KSPField] public float heatShieldBaseCost = 0.0f; // base part cost should be used here
+        [KSPField] public float heatShieldAreaCost = 0.0f;
+        [KSPField] public float heatShieldDiameterCost = 0.0f;
 
         #endregion KSPFields
 
@@ -23,9 +27,22 @@ namespace ROHeatshields
         private ModuleAblator modAblator;
         private ModuleROTank modularPart;
         private float hsMass = 0.0f;
+        private float hsCost = 0.0f;
+        private string ablatorResourceName;
+        private string outputResourceName;
 
         #endregion Private Variables
 
+        public float CurrentDiameter => modularPart?.currentDiameter ?? 0f;
+
+        public float HeatShieldMass => Mathf.Pow(CurrentDiameter, 2.0f) * heatShieldDensity;
+
+        public float HeatShieldAblator => Mathf.Round(Mathf.Pow(CurrentDiameter, 2.0f) * heatShieldAblator);
+
+        // Removes base part cost to replace it with our internal calculation instead.
+        // There's a heatShieldBaseCost fixed term, a diameter based linear term with coefficient HeatShieldDiameterCost,
+        // and a (diameter based) quadratic term with coefficient HeatShieldAreaCost.
+        public float HeatShieldCost => -part.partInfo.cost + heatShieldBaseCost + CurrentDiameter * heatShieldDiameterCost + Mathf.Pow(CurrentDiameter, 2.0f) * heatShieldAreaCost;
 
         #region Standard KSP Overrides
 
@@ -43,34 +60,68 @@ namespace ROHeatshields
             else
             {
                 if (modularPart is ModuleROTank)
+                {
                     modularPart.Fields[nameof(modularPart.currentDiameter)].uiControlEditor.onFieldChanged += OnDiameterChange;
+                }
+
+                ablatorResourceName = modAblator.ablativeResource;
+                outputResourceName = modAblator.outputResource;
+
                 UpdateHeatshieldValues();
             }
         }
 
         #endregion Standard KSP Overrides
 
-
         #region Custom Methods
 
         public ModifierChangeWhen GetModuleMassChangeWhen() => ModifierChangeWhen.FIXED;
         public float GetModuleMass(float defaultMass, ModifierStagingSituation sit) => Mathf.Max(0, hsMass);
 
-        private void OnDiameterChange(BaseField bf, object obj) => UpdateHeatshieldValues();
+        public ModifierChangeWhen GetModuleCostChangeWhen() => ModifierChangeWhen.FIXED;
+        public float GetModuleCost(float defaultCost, ModifierStagingSituation sit) => Mathf.Max(0, hsCost);
 
-        public float HeatShieldMass => Mathf.Pow(modularPart.currentDiameter, 2.0f) * heatShieldDensity;
-        public float HeatShieldAblator => Mathf.Round(Mathf.Pow(modularPart.currentDiameter, 2.0f) * heatShieldAblator);
+        private void OnDiameterChange(BaseField bf, object obj) => UpdateHeatshieldValues();
 
         public void UpdateHeatshieldValues()
         {
-            ROLLog.debug("UpdateHeatshieldValues()");
+            if(!HighLogic.LoadedSceneIsEditor)
+                return;
+
+            ROLLog.debug($"ROHeatshield {part.name} UpdateHeatshieldValues()");
             ROLLog.debug($"HeatShieldAblator: {HeatShieldAblator}");
             ROLLog.debug($"heatShieldDensity: {heatShieldDensity}");
             ROLLog.debug($"HeatShieldMass: {HeatShieldMass}");
-            part.Resources["Ablator"].maxAmount = part.Resources["Ablator"].amount = HeatShieldAblator;
-            part.Resources["CharredAblator"].maxAmount = HeatShieldAblator;
-            part.Resources["CharredAblator"].amount = 0;
+            ROLLog.debug($"HeatShieldCost: {HeatShieldCost}");
             hsMass = HeatShieldMass;
+            hsCost = HeatShieldCost;
+
+            if (part.Resources[ablatorResourceName] is PartResource ab)
+            {
+                double ratio = ab.maxAmount > 0 ? ab.amount / ab.maxAmount : 1.0;
+                ab.maxAmount = HeatShieldAblator;
+                ab.amount = Math.Min(ratio * ab.maxAmount, ab.maxAmount);
+            }
+
+            if (part.Resources[outputResourceName] is PartResource ca)
+            {
+                double ratio = ca.maxAmount > 0 ? ca.amount / ca.maxAmount : 1.0;
+                ca.maxAmount = HeatShieldAblator;
+                ca.amount = Math.Max(ratio * ca.maxAmount, 0);
+            }
+
+            DirtyPAW();
+        }
+
+        private void DirtyPAW()
+        {
+            foreach (UIPartActionWindow window in UIPartActionController.Instance.windows)
+            {
+                if (window.part == this.part)
+                {
+                    window.displayDirty = true;
+                }
+            }
         }
 
         #endregion Custom Methods
