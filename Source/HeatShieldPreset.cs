4fac3fcde327b1f48539357d255ac23a62f410de
Standecco <43384369+Standecco@users.noreply.github.com>
2021-05-30 08:45:06 +0200

    Fix attach nodes, Add DRE compatibility, Add preset switching (#1)
    * Persist ablator amount, add cost scaling
    
    * Add cost fields to the configs
    
    in RP-1, heatshield cost seems to scale linearly with diameter. Only exception: 10m diameter lunar rated heatshield, which costs 7200f rather than the expected 7500f.
    
    * Compile dll and remove local references
    
    * Fire ship modified event to ensure cost is updated
    
    * Fix misplaced nodes, build dll
    
    * Add preset switching and unify LEO and Lunar parts
    
    * Update project settings
    
    * Add more safety checks, DRE support, consider VScale in hs parameters
    
    * Update configs, build dll
    
    * Add Rider proj files, fix inverted flag check

---
+++ Source/HeatShieldPreset.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+
+namespace ROHeatshields
+{
+    public class HeatShieldPreset
+    {
+        public static readonly Dictionary<string, HeatShieldPreset> Presets = new Dictionary<string, HeatShieldPreset>();
+        public static bool Initialized { get; private set; } = false;
+
+        [Persistent] public string name = "";
+        [Persistent] public string description = "";
+        [Persistent] public bool disableModAblator = false;
+        
+        // procedural heat shield parameters
+        [Persistent] public float heatShieldDensity = 0.0f;
+        [Persistent] public float heatShieldAblator = 0.0f;
+        [Persistent] public float heatShieldBaseCost = 0.0f;
+        [Persistent] public float heatShieldAreaCost = 0.0f;
+        [Persistent] public float heatShieldDiameterCost = 0.0f;
+
+        // part parameters override
+        [Persistent] public float massOverride = -1;
+        [Persistent] public double maxTempOverride = -1f;
+        [Persistent] public double skinMaxTempOverride = -1f;
+
+        // ModuleAblator overrides
+        [Persistent] public string _ablativeResource;
+        [Persistent] public double _lossExp;
+        [Persistent] public double _lossConst;
+        [Persistent] public double _pyrolysisLossFactor;
+        [Persistent] public double _ablationTempThresh;
+        [Persistent] public double _reentryConductivity;
+        [Persistent] public bool _useNode;
+        [Persistent] public string _nodeName;
+        [Persistent] public float _charAlpha;
+        [Persistent] public float _charMax;
+        [Persistent] public float _charMin;
+        [Persistent] public bool _useChar;
+        [Persistent] public string _charModuleName;
+        [Persistent] public string _outputResource;
+        [Persistent] public double _outputMult;
+        [Persistent] public double _infoTemp;
+        [Persistent] public bool _usekg;
+        [Persistent] public string _unitsName;
+        [Persistent] public double _nominalAmountRecip;
+
+        public string AblativeResource;
+        public double? LossExp;
+        public double? LossConst;
+        public double? PyrolysisLossFactor;
+        public double? AblationTempThresh;
+        public double? ReentryConductivity;
+        public bool? UseNode;
+        public string NodeName;
+        public float? CharAlpha;
+        public float? CharMax;
+        public float? CharMin;
+        public bool? UseChar;
+        public string CharModuleName;
+        public string OutputResource;
+        public double? OutputMult;
+        public double? InfoTemp;
+        public bool? Usekg;
+        public string UnitsName;
+        public double? NominalAmountRecip;
+
+        public HeatShieldPreset(ConfigNode node)
+        {
+            node.TryGetValue("name", ref name);
+            node.TryGetValue("description", ref description);
+            node.TryGetValue("disableModAblator", ref disableModAblator);
+
+            node.TryGetValue("heatShieldDensity", ref heatShieldDensity);
+            node.TryGetValue("heatShieldAblator", ref heatShieldAblator);
+            node.TryGetValue("heatShieldBaseCost", ref heatShieldBaseCost);
+            node.TryGetValue("heatShieldDiameterCost", ref heatShieldDiameterCost);
+            node.TryGetValue("heatShieldAreaCost", ref heatShieldAreaCost);
+
+            node.TryGetValue("massOverride", ref massOverride);
+            node.TryGetValue("maxTempOverride", ref maxTempOverride);
+            node.TryGetValue("skinMaxTempOverride", ref skinMaxTempOverride);
+
+            if (node.TryGetValue("ablativeResource", ref _ablativeResource))
+                AblativeResource = _ablativeResource;
+            if (node.TryGetValue("lossExp", ref _lossExp))
+                LossExp = _lossExp;
+            if (node.TryGetValue("lossConst", ref _lossConst))
+                LossConst = _lossConst;
+            if (node.TryGetValue("pyrolysisLossFactor", ref _pyrolysisLossFactor))
+                PyrolysisLossFactor = _pyrolysisLossFactor;
+            if (node.TryGetValue("ablationTempThresh", ref _ablationTempThresh))
+                AblationTempThresh = _ablationTempThresh;
+            if (node.TryGetValue("reentryConductivity", ref _reentryConductivity))
+                ReentryConductivity = _reentryConductivity;
+            if (node.TryGetValue("useNode", ref _useNode))
+                UseNode = _useNode;
+            if (node.TryGetValue("nodeName", ref _nodeName))
+                NodeName = _nodeName;
+            if (node.TryGetValue("charAlpha", ref _charAlpha))
+                CharAlpha = _charAlpha;
+            if (node.TryGetValue("charMax", ref _charMax))
+                CharMax = _charMax;
+            if (node.TryGetValue("charMin", ref _charMin))
+                CharMin = _charMin;
+            if (node.TryGetValue("useChar", ref _useChar))
+                UseChar = _useChar;
+            if (node.TryGetValue("charModuleName", ref _charModuleName))
+                CharModuleName = _charModuleName;
+            if (node.TryGetValue("outputResource", ref _outputResource))
+                OutputResource = _outputResource;
+            if (node.TryGetValue("outputMult", ref _outputMult))
+                OutputMult = _outputMult;
+            if (node.TryGetValue("infoTemp", ref _infoTemp))
+                InfoTemp = _infoTemp;
+            if (node.TryGetValue("usekg", ref _usekg))
+                Usekg = _usekg;
+            if (node.TryGetValue("unitsName", ref _unitsName))
+                UnitsName = _unitsName;
+            if (node.TryGetValue("nominalAmountRecip", ref _nominalAmountRecip))
+                NominalAmountRecip = _nominalAmountRecip;
+        }
+
+        public HeatShieldPreset(string name)
+        {
+            this.name = name;
+        }
+
+        public static void LoadPresets()
+        {
+            if (Initialized && Presets.Count > 0)
+                return;
+
+            var nodes = GameDatabase.Instance.GetConfigNodes("ROHS_PRESET");
+            string s = string.Empty;
+            foreach (var node in nodes)
+            {
+                HeatShieldPreset preset = null;
+
+                if (node.TryGetValue("name", ref s) && !string.IsNullOrEmpty(s))
+                    preset = new HeatShieldPreset(node);
+
+                if (preset != null)
+                    Presets[preset.name] = preset;
+
+                UnityEngine.Debug.Log($"[ROHeatShields] Found and loaded preset {preset.name} ");
+            }
+
+            // initialize default fallback preset
+            if (!Presets.ContainsKey("default"))
+                Presets["default"] = new HeatShieldPreset("default");
+
+            Initialized = true;
+        }
+    }
+}
--- Source/ModuleROHeatshield.cs
+++ Source/ModuleROHeatshield.cs
@@ -1,20 +1,26 @@
-﻿using UnityEngine;
-using ROLib;
+﻿using ROLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ROHeatshields
 {
-    public class ModuleROHeatshield : PartModule, IPartMassModifier
+    public class ModuleROHeatshield : PartModule, IPartMassModifier, IPartCostModifier
     {
         private const string GroupDisplayName = "RO-Heatshields";
         private const string GroupName = "ModuleROHeatshield";
 
         #region KSPFields
 
-        [KSPField(guiActiveEditor = true, guiActive = true, guiName = "Type:", groupName = GroupName, groupDisplayName = GroupDisplayName)]
-        public string heatShieldType = "LEO";
+        [KSPField(isPersistant = true, guiName = "Type:", guiActiveEditor = true, groupName = GroupName, groupDisplayName = GroupDisplayName), UI_ChooseOption(scene = UI_Scene.Editor)]
+        public string heatShieldType = "default";
+        [KSPField(guiActiveEditor = true, guiName = "Description", groupName = GroupName, groupDisplayName = GroupDisplayName)]
+        public string description = "";
 
-        [KSPField] public float heatShieldDensity = 0.0f;
-        [KSPField] public float heatShieldAblator = 0.0f;
+        [KSPField(guiActiveEditor = true, guiName = "Max Temp", guiFormat = "F0", guiUnits = "K", groupName = GroupName, groupDisplayName = GroupDisplayName)]
+        public double maxTemp = 0.0f;
+        [KSPField(guiActiveEditor = true, guiName = "Max Skin Temp", guiFormat = "F0", guiUnits = "K", groupName = GroupName, groupDisplayName = GroupDisplayName)]
+        public double skinMaxTemp = 0.0f;
 
         #endregion KSPFields
 
@@ -23,54 +29,284 @@ namespace ROHeatshields
         private ModuleAblator modAblator;
         private ModuleROTank modularPart;
         private float hsMass = 0.0f;
+        private float hsCost = 0.0f;
+        private float origMass = 0.0f;
+        private float origCost = 0.0f;
+        private string ablatorResourceName;
+        private string outputResourceName;
+        [SerializeField] private string[] availablePresetsNames = new string[] { "default" };
 
         #endregion Private Variables
+        HeatShieldPreset ActivePreset => HeatShieldPreset.Presets[heatShieldType];
 
+        public float HeatShieldDensity => ActivePreset.heatShieldDensity;
+        public float HeatShieldBaseCost => ActivePreset.heatShieldBaseCost;
+        public float HeatShieldDiameterCost => ActivePreset.heatShieldDiameterCost;
+        public float HeatShieldAreaCost => ActivePreset.heatShieldAreaCost;
+
+        public float CurrentDiameter => modularPart?.currentDiameter ?? 0f;
+        public float CurrentVScale => modularPart?.currentVScale ?? 0f;
+        public float HeatShieldMass => (ActivePreset.massOverride > 0 ? -origMass + ActivePreset.massOverride : 0) + Mathf.Pow(CurrentDiameter, 2.0f) * HeatShieldDensity;
+        // Ablator = Round( D² * density * (vScale + 2)/2 )
+        public float HeatShieldAblator => Mathf.Round(Mathf.Pow(CurrentDiameter, 2.0f) * ActivePreset.heatShieldAblator * ((CurrentVScale + 2) / 2) * 10f) / 10f;
+
+        // Removes base part cost to replace it with our internal calculation instead.
+        // There's a heatShieldBaseCost fixed term, a diameter based linear term with coefficient HeatShieldDiameterCost,
+        // and a (diameter based) quadratic term with coefficient HeatShieldAreaCost.
+        public float HeatShieldCost => -origCost + HeatShieldBaseCost + CurrentDiameter * HeatShieldDiameterCost + Mathf.Pow(CurrentDiameter, 2.0f) * HeatShieldAreaCost;
 
         #region Standard KSP Overrides
+
+        public override void OnStart(StartState state)
+        {
+            HeatShieldPreset.LoadPresets();
+            if (!HeatShieldPreset.Initialized)
+                return;
+
+            if (availablePresetsNames.Length > 0 && HighLogic.LoadedSceneIsEditor)
+            {
+                string[] unlockedPresetsName = GetUnlockedPresets(availablePresetsNames);
+                UpdatePresetsList(unlockedPresetsName);
+                Fields[nameof(heatShieldType)].uiControlEditor.onFieldChanged =
+                Fields[nameof(heatShieldType)].uiControlEditor.onSymmetryFieldChanged =
+                    (bf, ob) => ApplyPreset(ActivePreset);
+            }
+
+            if (ActivePreset is null)
+            {
+                Debug.Log("[ROHeatshields] ActivePreset is null");
+                heatShieldType = "default";
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            if (node.TryGetValue("presets", ref availablePresetsNames))
+                Debug.Log("[ROHeatshields] available presets loaded");
+        }
 
         public override void OnStartFinished(StartState state)
         {
             base.OnStartFinished(state);
+
+            if (!HeatShieldPreset.Initialized)
+                return;
+
             modAblator = part.FindModuleImplementing<ModuleAblator>();
             modularPart = part.FindModuleImplementing<ModuleROTank>();
-            if (!(modAblator is ModuleAblator && modularPart is ModuleROTank))
+
+            if (modularPart is ModuleROTank)
             {
-                ROLLog.error($"{part} Unable to find ModuleAblator or ModuleROTank modules");
-                isEnabled = enabled = false;
-                return;
+                modularPart.Fields[nameof(modularPart.currentDiameter)].uiControlEditor.onFieldChanged += (bf, ob) => UpdateHeatshieldValues();
+                modularPart.Fields[nameof(modularPart.currentVScale)].uiControlEditor.onFieldChanged += (bf, ob) => UpdateHeatshieldValues();
+                modularPart.Fields[nameof(modularPart.currentDiameter)].uiControlEditor.onSymmetryFieldChanged += (bf, ob) => UpdateHeatshieldValues();
+                modularPart.Fields[nameof(modularPart.currentVScale)].uiControlEditor.onSymmetryFieldChanged += (bf, ob) => UpdateHeatshieldValues();
             }
-            else
-            {
-                if (modularPart is ModuleROTank)
-                    modularPart.Fields[nameof(modularPart.currentDiameter)].uiControlEditor.onFieldChanged += OnDiameterChange;
-                UpdateHeatshieldValues();
-            }
+
+            origCost = part.partInfo.cost + 0.01f;  // sum eps to avoid 0 cost
+            origMass = part.prefabMass + 0.00001f;  // sum eps to avoid 0 mass
+
+            ApplyPreset(ActivePreset);
         }
+
+        public ModifierChangeWhen GetModuleMassChangeWhen() => ModifierChangeWhen.FIXED;
+        public float GetModuleMass(float defaultMass, ModifierStagingSituation sit) => Mathf.Max(-origMass, hsMass);
+
+        public ModifierChangeWhen GetModuleCostChangeWhen() => ModifierChangeWhen.FIXED;
+        public float GetModuleCost(float defaultCost, ModifierStagingSituation sit) => Mathf.Max(-origCost, hsCost);
 
         #endregion Standard KSP Overrides
 
-
         #region Custom Methods
-
-        public ModifierChangeWhen GetModuleMassChangeWhen() => ModifierChangeWhen.FIXED;
-        public float GetModuleMass(float defaultMass, ModifierStagingSituation sit) => Mathf.Max(0, hsMass);
-
-        private void OnDiameterChange(BaseField bf, object obj) => UpdateHeatshieldValues();
-
-        public float HeatShieldMass => Mathf.Pow(modularPart.currentDiameter, 2.0f) * heatShieldDensity;
-        public float HeatShieldAblator => Mathf.Round(Mathf.Pow(modularPart.currentDiameter, 2.0f) * heatShieldAblator);
 
         public void UpdateHeatshieldValues()
         {
-            ROLLog.debug("UpdateHeatshieldValues()");
-            ROLLog.debug($"HeatShieldAblator: {HeatShieldAblator}");
-            ROLLog.debug($"heatShieldDensity: {heatShieldDensity}");
-            ROLLog.debug($"HeatShieldMass: {HeatShieldMass}");
-            part.Resources["Ablator"].maxAmount = part.Resources["Ablator"].amount = HeatShieldAblator;
-            part.Resources["CharredAblator"].maxAmount = HeatShieldAblator;
-            part.Resources["CharredAblator"].amount = 0;
             hsMass = HeatShieldMass;
+            hsCost = HeatShieldCost;
+
+            if(modularPart != null && modAblator != null && modAblator.enabled)
+            {
+                if (ablatorResourceName != null && part.Resources.Contains(ablatorResourceName))
+                {
+                    var ab = part.Resources[ablatorResourceName];
+                    double ratio = ab.maxAmount > 0 ? ab.amount / ab.maxAmount : 1.0;
+                    ab.maxAmount = HeatShieldAblator;
+                    ab.amount = Math.Min(ratio * ab.maxAmount, ab.maxAmount);
+                }
+                else
+                    Debug.Log("[ROHeatshields] " + (ablatorResourceName != null
+                        ? $"Resource {ablatorResourceName} not found!"
+                        : "Ablator Resource is null!"));
+
+                if (outputResourceName != null && part.Resources.Contains(outputResourceName))
+                {
+                    var ca = part.Resources[outputResourceName];
+                    double ratio = ca.maxAmount > 0 ? ca.amount / ca.maxAmount : 1.0;
+                    ca.maxAmount = HeatShieldAblator;
+                    ca.amount = Math.Max(ratio * ca.maxAmount, 0);
+                }
+                else
+                    Debug.Log("[ROHeatshields] " + (outputResourceName != null
+                        ? $"Resource {outputResourceName} not found!"
+                        : "Output Resource is null!"));
+            }
+
+            if (HighLogic.LoadedSceneIsEditor && EditorLogic.fetch?.ship != null)
+                GameEvents.onEditorShipModified.Fire(EditorLogic.fetch.ship);
+
+            UpdatePAW();
+        }
+
+        public void ApplyPreset(HeatShieldPreset p)
+        {
+            if (p is null)
+            {
+                Debug.Log("[ROHeatshields] invalid preset");
+                return;
+            }
+
+            ResetPartToOriginal();
+
+            if (p.maxTempOverride > 0)
+                part.maxTemp = p.maxTempOverride;
+            if (p.skinMaxTempOverride > 0)
+                part.skinMaxTemp = p.skinMaxTempOverride;
+
+            // update ModuleAblator parameters, if present and used
+            if(modAblator != null && !p.disableModAblator)
+            {
+                if (!string.IsNullOrWhiteSpace(p.AblativeResource))
+                    modAblator.ablativeResource = p.AblativeResource;
+                if (!string.IsNullOrWhiteSpace(p.OutputResource))
+                    modAblator.outputResource = p.OutputResource;
+
+                if (!string.IsNullOrWhiteSpace(p.NodeName))
+                    modAblator.nodeName = p.NodeName;
+                if (!string.IsNullOrWhiteSpace(p.CharModuleName))
+                    modAblator.charModuleName = p.CharModuleName;
+                if (!string.IsNullOrWhiteSpace(p.UnitsName))
+                    modAblator.unitsName = p.UnitsName;
+
+                if (p.LossExp.HasValue)
+                    modAblator.lossExp = p.LossExp.Value;
+                if (p.LossConst.HasValue)
+                    modAblator.lossConst = p.LossConst.Value;
+                if (p.PyrolysisLossFactor.HasValue)
+                    modAblator.pyrolysisLossFactor = p.PyrolysisLossFactor.Value;
+                if (p.AblationTempThresh.HasValue)
+                    modAblator.ablationTempThresh = p.AblationTempThresh.Value;
+                if (p.ReentryConductivity.HasValue)
+                    modAblator.reentryConductivity = p.ReentryConductivity.Value;
+                if (p.UseNode.HasValue)
+                    modAblator.useNode = p.UseNode.Value;
+                if (p.CharAlpha.HasValue)
+                    modAblator.charAlpha = p.CharAlpha.Value;
+                if (p.CharMax.HasValue)
+                    modAblator.charMax = p.CharMax.Value;
+                if (p.CharMin.HasValue)
+                    modAblator.charMin = p.CharMin.Value;
+                if (p.UseChar.HasValue)
+                    modAblator.useChar = p.UseChar.Value;
+                if (p.OutputMult.HasValue)
+                    modAblator.outputMult = p.OutputMult.Value;
+                if (p.InfoTemp.HasValue)
+                    modAblator.infoTemp = p.InfoTemp.Value;
+                if (p.Usekg.HasValue)
+                    modAblator.usekg = p.Usekg.Value;
+                if (p.NominalAmountRecip.HasValue)
+                    modAblator.nominalAmountRecip = p.NominalAmountRecip.Value;
+
+                ablatorResourceName = modAblator.ablativeResource;
+                outputResourceName = modAblator.outputResource;
+            }
+
+            if(modAblator != null)
+                modAblator.isEnabled = modAblator.enabled = !p.disableModAblator;
+
+            maxTemp = part.maxTemp;
+            skinMaxTemp = part.skinMaxTemp;
+
+            //prevent DRE from ruining everything
+            if(DREHandler.Found && HighLogic.LoadedSceneIsFlight)
+                DREHandler.SetOperationalTemps(part, maxTemp, skinMaxTemp);
+
+            if (!string.IsNullOrEmpty(p.description))
+            {
+                Fields[nameof(description)].guiActiveEditor = true;
+                description = p.description;
+            }
+            else
+                Fields[nameof(description)].guiActiveEditor = false;
+
+            Debug.Log($"[ROHeatshields] loaded preset {p.name} for part {part.name}");
+            UpdateHeatshieldValues();
+        }
+
+        public void ResetPartToOriginal()
+        {
+            Part prefab = part.partInfo.partPrefab;
+
+            part.maxTemp = prefab.maxTemp;
+            part.skinMaxTemp = prefab.skinMaxTemp;
+
+            if (modAblator is null)
+                return;
+
+            ConfigNode[] moduleNodes = part.partInfo.partConfig.GetNodes("MODULE");
+            int i = part.Modules.IndexOf(modAblator);
+            string moduleName = string.Empty;
+            if (i < moduleNodes.Length && moduleNodes[i].TryGetValue("name", ref moduleName) && moduleName == "ModuleAblator")
+            {
+                modAblator.Load(moduleNodes[i]);
+            }
+        }
+
+        public string[] GetUnlockedPresets(string[] all)
+        {
+            if (HighLogic.CurrentGame.Mode != Game.Modes.CAREER &&
+                HighLogic.CurrentGame.Mode != Game.Modes.SCIENCE_SANDBOX)
+            {
+                Debug.Log($"[ROHeatshields] All presets unlocked");
+                return all;
+            }
+
+            var unlocked = new List<string>();
+            foreach (var s in all)
+            {
+                if (PartUpgradeManager.Handler.GetUpgrade(s) is null || PartUpgradeManager.Handler.IsEnabled(s))
+                {
+                    Debug.Log($"[ROHeatshields] preset {s} is unlocked");
+                    unlocked.AddUnique(s);
+                }
+            }
+
+            if(unlocked.Count == 0)
+                unlocked.Add("default");
+
+            return unlocked.ToArray();
+        }
+
+        private void UpdatePresetsList(string[] options)
+        {
+            BaseField bf = Fields[nameof(heatShieldType)];
+
+            if (options.Length == 0) { options = new string[] { "NONE" }; }
+
+            var uiControlEditor = bf.uiControlEditor as UI_ChooseOption;
+            uiControlEditor.display = uiControlEditor.options = options;
+
+            Debug.Log($"[ROHeatshields] available presets on part {part.name}: " + string.Join(",", uiControlEditor.options));
+        }
+
+        public void UpdatePAW()
+        {
+            foreach (UIPartActionWindow window in UIPartActionController.Instance.windows)
+            {
+                if (window.part == this.part)
+                {
+                    window.displayDirty = true;
+                }
+            }
         }
 
         #endregion Custom Methods
